068b8923a08f92adf00f5ad0a1b9d75c340acd63
kivancguckiran <kivancguckiran@gmail.com>
2018-09-30 22:03:56 +0300

    added qu true changes and variance

--- min_util.py
+++ min_util.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 
 
 class bandit:
-    def __init__(self, eps=0, step_size=0., initial=0):
+    def __init__(self, eps=0, step_size=0., initial=0, variance=1):
         self.epsilon = eps
         self.step_size = step_size
-        self.time_step = 1
         self.k_arm = 5
+        self.variance = variance
 
         self.q_true = np.asarray([-0.25, 2, 1, -1.5, 3])
         self.q_estimates = np.zeros(self.k_arm) + initial
@@ -23,17 +23,19 @@ class bandit:
     def step(self):
         idx = self.action()
 
-        reward = self.q_true[idx] + np.random.randn()
+        self.actions_taken[idx] += 1
+
+        reward = self.q_true[idx] + np.random.randn() * self.variance
 
         if self.step_size == 0:
-            self.q_estimates[idx] += (1.0 / self.time_step) * (reward - self.q_estimates[idx])
+            self.q_estimates[idx] += (1.0 / self.actions_taken[idx]) * (reward - self.q_estimates[idx])
         else:
             self.q_estimates[idx] += self.step_size * (reward - self.q_estimates[idx])
 
-        self.actions_taken[idx] += 1
-        self.time_step += 1
-
         return reward
+
+    def change_q_true(self):
+        self.q_true = np.asarray([1, -0.5, -2, 2, 0.25])
 
     def take_steps(self, count):
         for _ in np.arange(count):
@@ -42,7 +44,7 @@ class bandit:
         self.showdown()
 
     def plot(self):
-        plt.violinplot(positions=np.arange(self.k_arm), dataset=self.q_true + np.random.randn(100,self.k_arm))
+        plt.violinplot(positions=np.arange(self.k_arm), dataset=self.q_true + np.random.randn(100,self.k_arm) * self.variance)
         plt.plot(self.q_estimates, color='red', marker='o', linestyle='', markersize=5)
         plt.show()
 
